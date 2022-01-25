f5a4faf31fe592f54041afba4c988d3f95afa411
GiovanniPerez <giorapni@gmail.com>
2022-01-25 12:39:26 -0600

    EstatusAlumnosADO

---
+++ INTRODUCCIÓN .NET Y C#/EstatusAlumnosADO/EstatusAlumnosADO/ADOEstatus.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstatusAlumnosADO
+{
+    internal class ADOEstatus : ICRUD
+    {
+        string conection = ConfigurationManager.ConnectionStrings["LocalConnection"].ConnectionString;
+        List<Estatus> _listEstatus;
+        string query;
+        SqlCommand comando;
+        public List<Estatus> Consultar()
+        {
+            query = "SELECT * FROM EstatusAlumnos";
+            using (SqlConnection con = new SqlConnection(conection))
+            {
+                comando = new SqlCommand(query, con);
+                comando.CommandType = CommandType.Text;
+                con.Open();
+                SqlDataReader reader = comando.ExecuteReader();
+                _listEstatus = new List<Estatus>();
+                while (reader.Read())
+                {
+                    _listEstatus.Add(
+                       new Estatus()
+                       {
+                           id = Convert.ToInt32(reader["idEstatusAlumnos"]),
+                           clave = reader["clave"].ToString(),
+                           nombre = reader["nombre"].ToString()
+                       }
+                       );
+                }
+                con.Close();
+            }
+            return _listEstatus;
+        }
+        public Estatus Consultar(int id)
+        {
+            Estatus estatusuno = new Estatus();
+            query = $"SELECT * FROM EstatusAlumnos WHERE idEstatusAlumnos = {id}";
+            using (SqlConnection con = new SqlConnection(conection))
+            {
+                comando = new SqlCommand(query, con);
+                comando.CommandType = CommandType.Text;
+                con.Open();
+                SqlDataReader reader = comando.ExecuteReader();
+                while (reader.Read())
+                {
+                    estatusuno = new Estatus { id = Convert.ToInt32(reader["idEstatusAlumnos"]),
+                        clave = reader["clave"].ToString(), nombre = reader["nombre"].ToString() };
+                }
+                con.Close();
+            }
+            return estatusuno;
+        }
+        public int Agregar(Estatus estatus)
+        {
+            int idES;
+            query = $"INSERT INTO EstatusAlumnos (clave, nombre) VALUES (@clave, @nombre)";
+            using (SqlConnection con = new SqlConnection(conection))
+            {
+                comando = new SqlCommand(query, con);
+                comando.CommandType = CommandType.Text;
+                comando.Parameters.AddWithValue("@clave", estatus.clave);
+                comando.Parameters.AddWithValue("@nombre", estatus.nombre);
+                con.Open();
+                idES = Convert.ToInt32(comando.ExecuteScalar());
+                con.Close();
+            }
+            return idES;
+        }
+            public void Actualizar(Estatus estatus)
+        {
+            query = $"UPDATE EstatusAlumnos SET clave = '{estatus.clave}', nombre = '{estatus.nombre}'" +
+                    $"WHERE idEstatusAlumnos = {estatus.id}";
+            using (SqlConnection con = new SqlConnection(conection))
+            {
+                comando = new SqlCommand(query, con);
+                comando.CommandType = CommandType.Text;
+                con.Open();
+                comando.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+        public void Eliminar(int id)
+        {
+            query = $"DELETE EstatusAlumnos WHERE idEstatusAlumnos = {id}";
+            using (SqlConnection con = new SqlConnection(conection))
+            {
+                comando = new SqlCommand(query, con);
+                comando.CommandType = CommandType.Text;
+                con.Open();
+                comando.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+    }
+}
---
+++ INTRODUCCIÓN .NET Y C#/EstatusAlumnosADO/EstatusAlumnosADO/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstatusAlumnosADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool salir = false;
+            string opc, menu;
+            menu = "\n1. Consultar todos\n2. Consultar solo uno\n3. Agregar\n4. Actualizar\n" +
+                   "5. Eliminar\n6. Terminar\n\nElija una opción:";
+            ADOEstatus bd = new ADOEstatus();
+            while (!salir)
+            {
+                Console.WriteLine(menu);
+                opc = Console.ReadLine();
+                if (opc == "1")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Consultar todos:\n");
+                    foreach (var i in bd.Consultar())
+                    {
+                        Console.WriteLine($"id: {i.id}, clave: {i.clave}, nombre: {i.nombre}");
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (opc == "2")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Consultar solo uno\n\nIngrese el id del estatus:");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    var item = bd.Consultar(id);
+                    Console.WriteLine($"id: {item.id}, clave: {item.clave}, nombre: {item.nombre}");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (opc == "3")
+                {
+                    bool otro = false;
+                    while (!otro)
+                    {
+                        Console.Clear();
+                        try
+                        {
+                            Console.WriteLine("Ingresa la clave del estatus");
+                            string clave = Console.ReadLine();
+                            Console.WriteLine("Ingresa la descripcion del estatus:");
+                            string nombre = Console.ReadLine();
+                            Estatus estatus = new Estatus { clave = clave, nombre = nombre };
+                            bd.Agregar(estatus);
+                            Console.WriteLine("\nRegistro realizado exitosamente.");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        Console.WriteLine("\n¿Desea agregar otro estatus? (y/n)");
+                        string opcion = Console.ReadLine();
+                        if (opcion == "n")
+                            otro = true;
+                        if (opcion == "N")
+                            otro = true;
+                    }
+                    Console.Clear();
+                }
+                else if (opc == "4")
+                {
+                    Console.Clear();
+                    try
+                    {
+                        Console.WriteLine("Actualizar\n\nIngrese el id del estatus a actualizar:");
+                        int idest = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingresa la nueva clave del estatus");
+                        string clave = Console.ReadLine();
+                        Console.WriteLine("Ingresa la nueva descripcion del estatus:");
+                        string nombre = Console.ReadLine();
+                        Estatus estatus = new Estatus { id = idest, clave = clave, nombre = nombre };
+                        bd.Actualizar(estatus);
+                        Console.WriteLine("\nEl estatus se actualizó correctamente");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (opc == "5")
+                {
+                    Console.Clear();
+                    try
+                    {
+                        Console.WriteLine("Eliminar\n\nIngrese el id del estatus a eliminar:");
+                        int id = int.Parse(Console.ReadLine());
+                        bd.Eliminar(id);
+                        Console.WriteLine("Eliminado correctamente.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (opc == "6")
+                    Environment.Exit(-1);
+                else
+                    Console.WriteLine("\nOpción no encontrada\n");
+            }
+        }
+    }
+}
